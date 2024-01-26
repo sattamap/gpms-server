929314bf5d862dae9a6d42bcd88800bbc77f763f
sattamap <sattamap@gmail.com>
2024-01-26 13:28:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db('gpms').collection('users');
+    const visitorCollection = client.db('gpms').collection('visitors');
     //Add endpoint to retrieve all users
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -178,8 +179,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
